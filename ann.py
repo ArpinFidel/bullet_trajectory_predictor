9fb2748e4a3ae5e07345c68dd0e9c20bec940953
ArpinFidel <richardputra2910@gmail.com>
2019-10-09 20:52:32 +0700

    added input

--- ann.py
+++ ann.py
@@ -7,7 +7,10 @@ from tensorflow import keras
 from keras import Sequential
 from keras.models import model_from_json
 from keras.layers import Dense
+from math import cos, sin
 
+model = None
+scaler = None
 
 def save_model():
     model_json = model.to_json()
@@ -44,9 +47,7 @@ def predict(angle, velocity, wind_angle, wind_speed):
     # print(x)
     return model.predict(x)
 
-try:
-    load_model()
-except:
+def train_model():
     fields = ['angle', 'velocity', 'wind_angle', 'wind_speed', 'range', 'windmil']
 
     raw = pd.read_csv('bullet.csv', names=fields, sep=',', skipinitialspace=True)
@@ -90,10 +91,44 @@ except:
         verbose=2
     )
 
+    
+try:
+    load_model()
+except:
+    train_model()
     save_model()
 
-prediction = predict(-89,407,199,18)
+if __name__ == '__main__':
+    while True:
+        print('1. train model')
+        print('2. predict')
+        print('3. exit')
+        while True:
+            try:
+                menu = int(input('>> '))
+            except:
+                continue
+            if 1<=menu<=2: break
+        
+        if menu == 3: break
+        if menu == 1:
+            train_model()
+        if menu == 2:
+            pos_x = input('position (x): ')
+            pos_y = input('position (y): ')
+            direction = input('direction (left right) [0 359]: ')
+            angle = input('angle (up down) [-90 90]: ')
+            velocity = input('bullet velocity [30 1400]: ')
+            wind_a = input('wind angle [0 359]: ')
+            wind_s = input('wind speed [0 114]: ')
 
-for p in prediction:
-    p[0], p[1] = (p[0]-scaler.min_[4])/scaler.scale_[4], (p[1]-scaler.min_[5])/scaler.scale_[5]
-    print('%.2f %.2f'%(p[0], p[1]))
+            prediction = predict(angle, velocity, wind_a, wind_s)
+
+            p = prediction[0]
+
+            p[0], p[1] = (p[0]-scaler.min_[4])/scaler.scale_[4], (p[1]-scaler.min_[5])/scaler.scale_[5]
+
+            D = p[0]
+            Q = direction + p[1]
+
+            print('%.2f %.2f'%(pos_x+D*cos(Q), pos_y+D*sin(Q)))
